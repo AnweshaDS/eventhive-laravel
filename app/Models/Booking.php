80435d5516e2582a80ac6ebc31c677fd1ac7536a
AnweshaDS <anwesha.das.bd.03@gmail.com>
2026-07-10 22:06:58 +0600

    Booking model user relationship is fixed and updated_at columns to all tables are added

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'ticket_type_id', 'quantity',
+        'total_amount', 'payment_status', 'booking_status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ticketType()
+    {
+        return $this->belongsTo(TicketType::class);
+    }
+
+    public function qrTicket()
+    {
+        return $this->hasOne(QrTicket::class);
+    }
 }
